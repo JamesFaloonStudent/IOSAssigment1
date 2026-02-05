6e903a194eb495976676e5614b95f79db41138e0
James Faloon <faloonj@oa-lc-29.ddi.sheridanc.on.ca>
2026-02-05 10:37:14 -0500

    created folder for view controllers and added a title

---
+++ Assigment1JamesFaloon/Assigment1JamesFaloon/ViewControllers/FormViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FormViewController.swift
+//  Assigment1JamesFaloon
+//
+//  Created by James Faloon on 2026-02-05.
+//
+
+import UIKit
+
+
+public class FormViewController : UIViewController {
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad();
+        title = "James Faloon Assigment 1";
+        
+        
+    }
+    
+    
+}
