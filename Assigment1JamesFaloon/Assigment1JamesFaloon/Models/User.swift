efeff76eaa9848aba962c9099f201161d372894c
James Faloon <faloonj@oa-lc-29.ddi.sheridanc.on.ca>
2026-02-05 10:59:41 -0500

    added a struct to manage User data with enough fields to support assigment

---
+++ Assigment1JamesFaloon/Assigment1JamesFaloon/Models/User.swift
@@ -0,0 +1,27 @@
+//  Ingredient.swift
+//  Assigment1JamesFaloon
+//
+//  Created by James Faloon on 2026-02-05.
+//
+
+
+
+// struct to soupport 8 text fields 3 picker views a Switch and a Slider
+struct User {
+    let FirstName : String = "";
+    let LastName = "";
+    let Email = "";
+    let Password = "";
+    let PhoneNumber = "";
+    let address : String = "";
+    let city : String = "";
+    let Username : String = "";
+    
+    let InIosClass : Bool = false;
+    
+    let experinceLevel : Float = 0.0;
+    
+    let selectedGender : String = "Male";
+    let selectedCountry : String = "Canada";
+    let selectedAccountType : String = "Basic";
+}
