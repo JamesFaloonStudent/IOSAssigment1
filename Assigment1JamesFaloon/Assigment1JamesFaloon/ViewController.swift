f33e20c055ad12d99eb7b85bff4ec99bd040ea7e
James Faloon <faloonj@oa-lc-29.ddi.sheridanc.on.ca>
2026-02-05 10:16:54 -0500

    added gitignore file

---
+++ Assigment1JamesFaloon/Assigment1JamesFaloon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assigment1JamesFaloon
+//
+//  Created by James Faloon on 2026-02-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
